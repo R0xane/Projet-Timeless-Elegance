0448dd79ece04e25f5d15eebff0c9b4f6340717c
Rolwen <n.rnolwen@gmail.com>
2025-07-18 02:13:46 +0200

    user can only enter simulation with valid mail

--- Assets/Scripts/Intro/ShowKeyboard.cs
+++ Assets/Scripts/Intro/ShowKeyboard.cs
@@ -6,22 +6,53 @@ public class ShowKeyboard : MonoBehaviour
 {
     private TMP_InputField inputField;
 
+    [Header("Play Button")]
+    public GameObject playButton;
+
     void Start()
     {
         inputField = GetComponent<TMP_InputField>();
         inputField.onSelect.AddListener(x => OpenKeyboard());
+
+        if (playButton != null)
+        {
+            playButton.SetActive(false);
+        }
+
+        if (NonNativeKeyboard.Instance != null)
+        {
+            NonNativeKeyboard.Instance.OnTextSubmitted += OnKeyboardTextSubmitted;
+        }
     }
 
     public void OpenKeyboard()
     {
         NonNativeKeyboard.Instance.InputField = inputField;
-        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
-
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text, NonNativeKeyboard.LayoutType.Email);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (NonNativeKeyboard.Instance != null)
+        {
+            NonNativeKeyboard.Instance.OnTextSubmitted -= OnKeyboardTextSubmitted;
+        }
+    }
+
+    private void OnKeyboardTextSubmitted(object sender, System.EventArgs e)
+    {
+        string submittedText = inputField.text;
+        bool isValid = IsValidEmail(submittedText);
+
+        if (playButton != null)
+        {
+            playButton.SetActive(isValid);
+        }
+    }
+
+    private bool IsValidEmail(string email)
+    {
+        return email.Contains("@") && email.Contains(".");
     }
 }
+
